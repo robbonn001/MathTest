66624cd5500ac763138705b4eb61ba3a14120ab8
IgorVif <robbonn001@gmail.com>
2026-09-10 13:59:28 +0300

    Пишу класс Task

--- Lib/MathTest.cpp
+++ Lib/MathTest.cpp
@@ -1,1 +1,26 @@
 #include "MathTest.h"
+
+Task::Task() {
+	num_1 = rand() % 100;
+	num_2 = rand() % 100;
+	int num = rand()%4;
+	switch (num) {
+	case 0: operation = '+'; break;
+	case 1: operation = '-'; break;
+	case 2: operation = '*'; break;
+	case 3: operation = '/'; break;
+	}
+}
+
+Task::Task(int min, int max, char operation_ = '\0'){
+	num_1 = rand() % (max - min) + min;
+	num_2 = rand() % (max - min) + min;
+	
+	switch (num) {
+	case 0: operation = '+'; break;
+	case 1: operation = '-'; break;
+	case 2: operation = '*'; break;
+	case 3: operation = '/'; break;
+	}
+}
+
--- Lib/MathTest.h
+++ Lib/MathTest.h
@@ -1,5 +1,13 @@
 #pragma once
-struct Task;
+#include <cmath>
+struct Task {
+	int num_1;
+	int num_2;
+	char operation;
+	double answer;
+	Task();
+	Task(int min, int max, char operation_ = '\0');
+};
 class MathTest;
 
 
