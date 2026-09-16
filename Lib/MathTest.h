49a72cb9628148b7c2d269c4ca1c1734e5d474df
IgorVif <robbonn001@gmail.com>
2026-09-17 02:10:37 +0300

    Описан класс MathTest

--- Lib/MathTest.h
+++ Lib/MathTest.h
@@ -10,4 +10,44 @@ struct Task {
 	Task();
 	Task(int, int, char operation_ = '\0');
 };
-class MathTest;
+class MathTest {
+	Task* tasks;
+	int count;
+	int* user_answers;
+	int correct_count;
+
+	// Вспомогательный метод для инициализации памяти
+	void allocateMemory(int size);
+
+	// Вспомогательный метод для очистки памяти
+	void freeMemory();
+
+public:
+	MathTest();
+	MathTest(int size, int min, int max);
+	MathTest(int size, int min, int max, char operation);
+
+	~MathTest();
+
+	MathTest(const MathTest&) = delete;
+	MathTest& operator=(const MathTest&) = delete;
+
+	void run();
+
+	void showStatistics() const;
+
+	// Принять ответ пользователя и обновить статистику
+	bool submitAnswer(int index, int answer);
+
+	const Task& getTask(int index) const;
+
+	int getUserAnswer(int index) const;
+
+	int getCorrectCount() const;
+
+	// Получить общее количество вопросов
+	int getCount() const;
+
+	// Вычислить оценку на основе процента правильных ответов
+	static char calculateMark(int correct, int total);
+};
